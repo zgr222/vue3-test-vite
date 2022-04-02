09b320e2f7e05a2b770556e4a378969cd4b6d93d
joe <529268260@qq.com>
2022-04-02 11:06:15 +0800

    init a project

---
+++ demo-two/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>ok</h1>
+  <CheckEditor />
+</template>
+
+<script>
+import CheckEditor from "./components/CheckEditor.vue";
+export default {
+  components: { CheckEditor },
+};
+</script>
+
+<style>
+</style>
