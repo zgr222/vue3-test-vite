30a3a84d2678272531cf3d22d07a8b786a9b0c96
joe <529268260@qq.com>
2022-04-07 15:50:29 +0800

    完成共享数据方法之一demo-provide-inject

---
+++ 共享数据/demo-provide-inject/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<script>
+import { inject } from "vue";
+export default {
+  setup() {
+    const foo = inject("foo");
+    console.log("home", foo);
+  },
+};
+</script>
